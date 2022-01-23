7c3bc162e51685c69ecbf3b8200ed92bab58194e
Denis <busindenis@gmail.com>
2022-01-24 01:23:29 +0300

    Lesson3

---
+++ MyVKtest/MyVKtest/FriendList/Model/FriendsListCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  FriendsListCellModel.swift
+//  MyVKtest
+//
+//  Created by Денис Бусурин on 23.01.2022.
+//
+
+import Foundation
+
+struct FriendsListCellModel {
+    let name: String
+    let surnname: String
+    let imageName: String
+}
---
+++ MyVKtest/MyVKtest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyVKtest
+//
+//  Created by Денис Бусурин on 23.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func didTapOnButton(_ sender: UIButton) {
+        
+        
+        print(" 111 :) :) :) ")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
